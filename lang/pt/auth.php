ce5bfa6b1d0593aaf48cc35a278a2a2f4a22f38c
viniciuscribeiro <viniciuscribeiro@gmail.com>
2025-11-06 00:16:06 -0300

    Polimento: Traduz a aplicação para PT-BR e simplifica a interface de autenticação

---
+++ lang/pt/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Linguagem de Autenticação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de linguagem são usadas durante a autenticação para várias
+    | mensagens que precisamos exibir ao usuário. Você é livre para modificar
+    | essas linhas de acordo com os requisitos da sua aplicação.
+    |
+    */
+
+    'failed'   => 'Estas credenciais não correspondem aos nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,19 +14,14 @@
             <x-input-label for="password" :value="__('Senha')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                type="password"
+                name="password"
+                required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Lembrar-me') }}</span>
-            </label>
-        </div>
+
 
         <div class="flex items-center justify-between mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
@@ -34,11 +29,6 @@
             </a>
 
             <div>
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Esqueceu sua senha?') }}
-                    </a>
-                @endif
 
                 <x-primary-button class="ms-4">
                     {{ __('Entrar') }}
